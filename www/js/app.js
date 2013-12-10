c223d6743cca2f4dd92e35e9d9bb7600e4d834d0
Ch AFleks <adynetro@yahoo.com>
2013-12-10 17:16:08 +0200

    fortat

---
+++ www/js/app.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', function () {
+    new FastClick(document.body);
+}, false);
+
+// The dynamically built HTML pages. In a real-life app, In a real-life app, use Handlerbar.js, Mustache.js or another template engine
+var slider = new PageSlider($("#container"));
+$(window).on('hashchange', route);
+
+// Basic page routing
+function route(event) {
+    var page,
+        hash = window.location.hash;
+
+    if (hash === "#locatie") {
+        page = $("#container").load("location.html") ;
+//        slider.slide($(page), "right");
+    } else if (hash === "#page2") {
+        page = merge(detailsPage, {img: "medibot.jpg", name: "Medi Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+//        slider.slide($(page), "right");
+    } else if (hash === "#page3") {
+        page = merge(detailsPage, {img: "ripplebot.jpg", name: "ripplebot Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+//        slider.slide($(page), "right");
+    }
+    else {
+        page = $("#container").load("collectin.html") ;
+;
+//        slider.slide($(homePage), "left");
+    }
+
+    slider.slidePage($(page));
+
+}
+
+// Primitive template processing. In a real-life app, use Handlerbar.js, Mustache.js or another template engine
+function merge(tpl, data) {
+    return tpl.replace("{{img}}", data.img)
+              .replace("{{name}}", data.name)
+              .replace("{{description}}", data.description);
+}
+
+route();
